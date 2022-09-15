94cd78a56d276945c7189fd281b9f7989d5f951e
barissemerci <barissemerci3@gmail.com>
2022-09-15 20:49:15 +0300

    LoginScreen.js added

---
+++ SpotifyClone/src/screens/LoginScreen.js
@@ -0,0 +1,91 @@
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import {auth} from '../../firebase'
+const LoginScreen = () => {
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+
+
+  const handleSignup=()=>{
+    auth.createUserWithEmailAndPassword(email,password).then(
+      userCredentials=>{
+        const user=userCredentials.user;
+        console.log("REGISTERED",email)
+      }
+    ).catch(error=>alert(error.message))
+  }
+
+  const handleLogin=()=>{
+    auth.signInWithEmailAndPassword(email,password).then(
+      userCredentials=>{
+        const user=userCredentials.user;
+        console.log("LOGGED IN ",email)
+      }
+    ).catch(error=>alert(error.message))
+  }
+
+
+
+  return (
+<KeyboardAvoidingView style={styles.container} > 
+  <View style={styles.inputContainer}>
+  <TextInput placeholder='Email'
+  value={email}
+  onChangeText={text=> setEmail(text)}
+  style={styles.input}
+  />
+
+  <TextInput placeholder='Password'
+  value={password}
+  onChangeText={text=>setPassword(text) }
+  style={styles.input}
+  secureTextEntry
+  />
+  
+  </View>    
+
+  <View style={styles.buttonContainer}>
+
+    <TouchableOpacity onPress={()=>{}} style={styles.button}>
+      <Text style={styles.buttonTextStyle}>Login</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress={handleSignup} style={[styles.button,styles.buttonOutline]}>
+      <Text style={styles.buttonTextStyle}>Register</Text>
+    </TouchableOpacity>
+
+  </View>
+</KeyboardAvoidingView>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+
+  container:{
+    backgroundColor:'red',
+    justifyContent:'center',
+    alignItems:'center',
+    flex:1
+  },
+  input:{
+    backgroundColor:'white',
+  paddingHorizontal:15,
+paddingVertical:10,
+borderRadius:10,
+marginTop:5},
+  inputContainer:{ width:'80%'},
+  buttonContainer:{width:'60%',
+  justifyContent:'center',
+  alignItems:'center',
+  marginTop:40
+},
+  button:{backgroundColor:'green',
+width:'100%',
+padding:15,
+borderRadius:10,
+alignItems:'center'},
+  buttonOutline:{backgroundColor:'white',marginTop:5,borderWidth:2,borderColor:'green'},
+  buttonTextStyle:{}
+})
