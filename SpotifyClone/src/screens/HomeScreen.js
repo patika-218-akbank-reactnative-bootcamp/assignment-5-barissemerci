27c64e23143f7ed59468e46bec7db8e4ebf8dcd3
barissemerci <barissemerci3@gmail.com>
2022-09-15 21:53:15 +0300

    signout

--- SpotifyClone/src/screens/HomeScreen.js
+++ SpotifyClone/src/screens/HomeScreen.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../../firebase'
+import { useNavigation } from '@react-navigation/core'
 
 const HomeScreen = () => {
+    const navigation= useNavigation()
+
+
+const handleSignout=()=>{
+    auth.signOut().then(()=>{
+        navigation.replace('LoginScreen')}).catch(error=>alert(error.message))
+    
+}
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text>Email {auth.currentUser?.email}</Text>
+
+
+      <TouchableOpacity onPress={handleSignout} style={styles.button}>
+        <Text >Sign Out</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+},
+button:{backgroundColor:'green',
+width:'60%',
+padding:15,
+borderRadius:10,
+alignItems:'center',
+marginTop:40},
+
+
+
+})
