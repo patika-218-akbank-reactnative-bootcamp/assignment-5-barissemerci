1a80cf0b253928e00626a26d71bd1911c5b32384
barissemerci <barissemerci3@gmail.com>
2022-09-16 10:57:05 +0300

    AsyncStorage feaure added

--- SpotifyClone/src/screens/LoginScreen.js
+++ SpotifyClone/src/screens/LoginScreen.js
@@ -1,16 +1,39 @@
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState,useEffect } from 'react'
 import {auth} from '../../firebase'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { useNavigation } from '@react-navigation/core'
 
 const LoginScreen = () => {
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('') 
   const navigation= useNavigation()
+
+
+  const storeData = async value => {
+    try {
+      console.log("value.email",value.email)
+      const jsonValue = JSON.stringify(value);
+
+      await AsyncStorage.setItem('user', jsonValue);
+
+      console.log("LoginScreen AsyncStorage'a atıldı:", jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+
   useEffect(() => {
    const unsubscribe= auth.onAuthStateChanged(user=>{
         if(user){
             navigation.replace('HomeScreen')
+            storeData(user)
+            AsyncStorage.getItem('user').then(val => {
+                console.log("MainStackNavigation AsyncStorage'dan çekildi:", val.phoneNumber);
+              
+              });
         }
     }
         )
@@ -33,7 +56,13 @@ const LoginScreen = () => {
     auth.signInWithEmailAndPassword(email,password).then(
       userCredentials=>{
         const user=userCredentials.user;
+        storeData(user)
         console.log("LOGGED IN ",email)
+        AsyncStorage.getItem('user').then(val => {
+            console.log("MainStackNavigation AsyncStorage'dan çekildi:", val);
+          
+          });
+
       }
     ).catch(error=>alert(error.message))
   }
