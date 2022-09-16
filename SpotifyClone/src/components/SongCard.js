12a60eb6538e98bc55407de942c26966560f2968
barissemerci <barissemerci3@gmail.com>
2022-09-16 12:52:48 +0300

    SongCard updated

--- SpotifyClone/src/components/SongCard.js
+++ SpotifyClone/src/components/SongCard.js
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 
-const SongCard = ({name}) => {
+const SongCard = ({photo,singer,name}) => {
   return (
     <View style={styles.container}>
-      <Text>deneme</Text>
+        <Image style={styles.imageStyle}  
+        source={{
+              uri: 'https://e-cdn-images.dzcdn.net/images/artist/' + photo+'/264x264-000000-80-0-0.jpg'
+            }} />
+                <View>
+                <Text>{name}</Text>
+      <Text>{singer}</Text>
+                </View>
+     
     </View>
   )
 }
@@ -13,6 +21,15 @@ export default SongCard
 
 const styles = StyleSheet.create({
     container:{
+        flexDirection:'row',
+        margin:10,
+        borderWidth:1,
+        borderColor:'black',
+        padding:5
 
+    },
+    imageStyle:{
+        width:100,height:100,
+        borderRadius:50
     }
 })
