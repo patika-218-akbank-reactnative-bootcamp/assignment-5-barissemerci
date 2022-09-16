0f3c45eddfdffa0b2fa470bf28e215d58ce6f887
barissemerci <barissemerci3@gmail.com>
2022-09-16 13:13:21 +0300

    GenreSongScreen added

---
+++ SpotifyClone/src/screens/GenreSongScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GenreSongScreen = () => {
+  return (
+    <View>
+      <Text>GenreSongScreen</Text>
+    </View>
+  )
+}
+
+export default GenreSongScreen
+
+const styles = StyleSheet.create({})
