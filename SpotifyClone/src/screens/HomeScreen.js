bc9f5c5514aeb535e92a09d7aa49365660c2c945
barissemerci <barissemerci3@gmail.com>
2022-09-16 10:57:41 +0300

    genres list and top tracks list added

--- SpotifyClone/src/screens/HomeScreen.js
+++ SpotifyClone/src/screens/HomeScreen.js
@@ -1,27 +1,85 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { auth } from '../../firebase'
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React,{useState,useEffect} from 'react'
 import { useNavigation } from '@react-navigation/core'
+import axios from 'axios';
+import GenreCard from '../components/GenreCard';
+import SongCard from '../components/SongCard';
+
 
 const HomeScreen = () => {
     const navigation= useNavigation()
+    const [genreList,setGenreList]=useState(null)
+    const [topTracks,setTopTracks]=useState(null)
 
+    const renderGenreCard=({item})=>{
+      return(
+       <GenreCard name={item.name}></GenreCard>
+      )
+    }
 
-const handleSignout=()=>{
-    auth.signOut().then(()=>{
-        navigation.replace('LoginScreen')}).catch(error=>alert(error.message))
+  const  renderSongCard=({item})=>{
+      return(
+        <SongCard ></SongCard>
+      )
+    }
+
+    useEffect(() => {
+      getGenres()
+      getTopTracks()
     
-}
+    }, [])
+    
+
+    const getGenres = () => {
+      axios
+        .get(
+          'https://api.deezer.com/genre'
+        )
+        .then(response => {
+          setGenreList(response.data.data.slice(0,4));
+          console.log("genreList",genreList)
+
+        });
+    };
+
+
+    const getTopTracks= ()=>{
+      axios
+      .get(
+        'https://api.deezer.com/chart/0/tracks'
+      )
+      .then(response => {
+        setTopTracks(response.data.data);
+
+      });
+
+
+    }
+
 
   return (
-    <View style={styles.container}>
-      <Text>Email {auth.currentUser?.email}</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titleTextStyle}>Genres</Text>
+      <View style={styles.flatListContainer}>
+      <FlatList style={styles.flatListStyle}
+       data={genreList}
+       keyExtractor={item => item.id}
+       renderItem={renderGenreCard}
+       numColumns={2}
+       columnWrapperStyle={styles.row}
+     />
+      </View>
+      <Text>Top Tracks</Text>
+      <FlatList style={styles.flatListStyle}
+       data={topTracks}
+       keyExtractor={item => item.id}
+       renderItem={renderSongCard} />
+      
 
+      
 
-      <TouchableOpacity onPress={handleSignout} style={styles.button}>
-        <Text >Sign Out</Text>
-      </TouchableOpacity>
-    </View>
+     
+    </SafeAreaView>
   )
 }
 
@@ -29,16 +87,24 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
 container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+width:'100%',height:'100%',
+    
 },
-button:{backgroundColor:'green',
-width:'60%',
-padding:15,
-borderRadius:10,
-alignItems:'center',
-marginTop:40},
+titleTextStyle:{fontSize:30,fontWeight:'bold',marginTop:30,
+marginLeft:20},
+flatListStyle:{
+  backgroundColor:'gray',
+  flexGrow: 0,
+
+},
+flatListContainer:{
+width:'100%'
+
+},
+row: {
+  flex: 1,
+  justifyContent: "space-around"
+}
 
 
 
